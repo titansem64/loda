22dcca350ed31c417df4659e198d04c282c0492f
titansem64 <titansem64+1@gmail.com>
2025-01-02 09:49:40 +0530

    Update config (3).py

--- config (3).py
+++ config (3).py
@@ -1,1 +1,6 @@
-404: Not Found
+BOT_TOKEN = "8022705558:AAFIkcDxK0w2HtkXfPKlraQRTqK4L63Mg-o"
+ADMIN_IDS = ["7163028849"]
+GROUP_ID = -1002175044331  # Replace with your group ID
+GROUP_LINK = "https://t.me/+fxw0jsLZ4203ODdl"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 25  # Set the default number of threads to use for attacks
